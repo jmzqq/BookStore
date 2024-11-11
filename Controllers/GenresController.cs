54a38cb0edff145b37ffe7ea1253dc4fe8eae243
jmzqq <joaomwguel@gmail.com>
2024-11-11 14:35:17 -0300

    feat: implements index and create genre features

---
+++ Controllers/GenresController.cs
@@ -0,0 +1,45 @@
+﻿using Bookstore.Data;
+using Bookstore.Models;
+using Bookstore.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+	public class GenresController : Controller
+	{
+		private readonly GenreService _service;
+
+		public GenresController(GenreService service)
+		{
+			_service = service;
+		}
+
+		public IActionResult Index()
+		{
+			return View(_service.FindAll());
+		}
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(Genre genre)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View();
+			}
+
+			_service.Insert(genre);
+
+			return RedirectToAction(nameof(Index));
+		}
+
+
+
+
+	}
+}
---
+++ Models/Genre.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookstore.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Name { get; set; }
+        //public ICollection<Book> Books { get; set; } = new List<Book>();
+
+        public Genre()
+        {
+
+        }
+
+        public Genre(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+
+
+    }
+}
